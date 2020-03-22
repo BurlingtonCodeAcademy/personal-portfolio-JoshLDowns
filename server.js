98f314563ad9b7f6abd3d022389f474bdfd3b04d
JoshLDowns <josh.lloyd.downs@gmail.com>
2020-03-22 19:43:28 -0400

    server update

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000
 
-app.use(express.static(path.resolve('./client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve('./client/public/index.html'))
-})
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
 
 app.listen(port, ()=>{console.log(`Listening on port: ${port}`)})
